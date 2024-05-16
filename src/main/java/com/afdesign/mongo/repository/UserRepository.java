e28700a35a37285a59f7a5ac1c9097fc816278b4
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-16 17:04:58 -0300

    feat: instantiation created and settings

--- src/main/java/com/afdesign/mongo/repository/UserRepository.java
+++ src/main/java/com/afdesign/mongo/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.afdesign.mongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.afdesign.mongo.domain.User;
 
-public interface UserRepository extends MongoRepository<User, String>{
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
 
 }
