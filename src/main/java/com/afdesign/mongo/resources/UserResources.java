fcbf4270048be576bf752058484f76264494b54c
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-05-17 14:01:26 -0300

    feat: find user by id implemented and their excpetions handling

--- src/main/java/com/afdesign/mongo/resources/UserResources.java
+++ src/main/java/com/afdesign/mongo/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,12 @@ public class UserResources {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(u->new UserDTO(u)).toList();
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+
+	@GetMapping (value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/afdesign/mongo/services/UserService.java
+++ src/main/java/com/afdesign/mongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.afdesign.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.afdesign.mongo.domain.User;
 import com.afdesign.mongo.repository.UserRepository;
+import com.afdesign.mongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repo; 
-	
-	public List<User> findAll(){
+	private UserRepository repo;
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
